448aacb3fe30229b6495f2aed0a8c90ad6500046
Samuel Victor <samuel@samuelvictor.dev>
2025-06-01 21:16:11 -0300

    remove commented blocks

--- lambda.tf
+++ lambda.tf
@@ -4,34 +4,6 @@ resource "aws_ssm_parameter" "jwt_secret" {
   value       = "PLACEHOLDER"  # Trocar para o valor real do JWT secret
 }
 
-# resource "aws_iam_role_policy" "lambda_policy" {
-#   name = "lambda-auth-cpf-policy"
-#   role = var.labRole
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect   = "Allow",
-#         Action   = [
-#           "ssm:GetParameter",
-#           "ssm:GetParameters"
-#         ],
-#         Resource = aws_ssm_parameter.jwt_secret.arn
-#       },
-#       {
-#         Effect   = "Allow",
-#         Action   = [
-#           "logs:CreateLogGroup",
-#           "logs:CreateLogStream",
-#           "logs:PutLogEvents"
-#         ],
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
-
 resource "aws_lambda_function" "auth_by_cpf" {
   function_name    = "auth-by-cpf"
   filename         = "./lambda.zip"
